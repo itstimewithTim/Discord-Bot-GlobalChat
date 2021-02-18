675247809411b7481c045d0be1f6d2609194a25e
Tim <itstimewithtim@gmail.com>
2021-02-17 23:20:07 -0800

    Erased the (outdated) token from code.
    As soon as I committed this code, Discord sent me a notification saying that they detected that I had publicly posted my Bot's Token (facepalm*). Not only that, they also automatically reset the token. I thought I should still come here and erase it though haha

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ import cog_globalchat
 #Create the client object
 bot = commands.Bot(command_prefix='g?')
 
-TOKEN = "Nzg3MDc1NzQ4NTI2OTQ4Mzcy.X9PraQ.2qybqvZxxd1K-ghFuB3eCHYv42M"
-#os.environ['DTOKEN2'] #This will reference the DTOKEN2 that was defined in my .bashrc
+TOKEN = "enter Bot's token here"
 
 # Terminate the bot when the command g?exit is run
 @bot.command(name='exit')
